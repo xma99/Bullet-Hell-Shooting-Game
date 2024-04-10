84a8f11443fcc73fa83e631725b2c4bd866730b6
tplink329242 <y947261692@outlook.com>
2024-04-10 00:01:38 -0700

    sync uml

--- Alpha Danmaku Rush Demo/obj/Debug/net6.0/Alpha Danmaku Rush Demo - Backup (1).AssemblyInfo.cs
+++ Alpha Danmaku Rush Demo/obj/Debug/net6.0/Alpha Danmaku Rush Demo - Backup (1).AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Alpha Danmaku Rush Demo")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Alpha Danmaku Rush Demo - Backup (1)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d25435b9ffdc985943144159209842461dc42442")]
-[assembly: System.Reflection.AssemblyProductAttribute("Alpha Danmaku Rush Demo")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Alpha Danmaku Rush Demo")]
+[assembly: System.Reflection.AssemblyProductAttribute("Alpha Danmaku Rush Demo - Backup (1)")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Alpha Danmaku Rush Demo - Backup (1)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
