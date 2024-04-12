39aece134fe69ee46a6fbefbbb05011566c8d325
tplink329242 <y947261692@outlook.com>
2024-04-12 16:46:34 -0700

    update code

--- Alpha Danmaku Rush Demo/obj/Debug/net6.0/Alpha Danmaku Rush Demo.AssemblyInfo.cs
+++ Alpha Danmaku Rush Demo/obj/Debug/net6.0/Alpha Danmaku Rush Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alpha Danmaku Rush Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da326f87ae8ce3f71c75893ce15ceda65fe1f244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac8383b5a90ea18dbde3a985f1d6632ef443e2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alpha Danmaku Rush Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alpha Danmaku Rush Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
