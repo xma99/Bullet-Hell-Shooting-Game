ac14376f3dd70f5dcd99c101e4398c29cd693330
tplink329242 <y947261692@outlook.com>
2024-04-25 18:47:35 -0700

    sync

--- Alpha Danmaku Rush Demo/obj/Debug/net6.0/Alpha Danmaku Rush Demo.AssemblyInfo.cs
+++ Alpha Danmaku Rush Demo/obj/Debug/net6.0/Alpha Danmaku Rush Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alpha Danmaku Rush Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac8383b5a90ea18dbde3a985f1d6632ef443e2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39aece134fe69ee46a6fbefbbb05011566c8d325")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alpha Danmaku Rush Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alpha Danmaku Rush Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
